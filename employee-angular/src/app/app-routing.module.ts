7166bd2e50b29c27bb0ea6357afc8d1591c13969
nav2020 <nav2020@hotmail.com>
2020-05-09 11:20:47 -0400

    Refactored so that both the sub projects have meaningful names

---
+++ employee-angular/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { EmployeeDetailsComponent } from './employees/employee-details/employee-details.component';
+import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './employees/update-employee/update-employee.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'add', component: CreateEmployeeComponent },
+  { path: 'update/:id', component: UpdateEmployeeComponent },
+  { path: 'details/:id', component: EmployeeDetailsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ employee-angular/src/app/employees/update-employee/update-employee.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-update-employee',
+  templateUrl: './update-employee.component.html',
+  styleUrls: ['./update-employee.component.css']
+})
+export class UpdateEmployeeComponent implements OnInit {
+
+  id: number;
+  employee: Employee;
+  submitted = false;
+  constructor(private route: ActivatedRoute,private router: Router,
+    private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.employee = new Employee();
+    this.id = this.route.snapshot.params['id'];
+    
+    this.employeeService.getEmployee(this.id)
+      .subscribe(data => {
+        console.log(data)
+        this.employee = data;
+      }, error => console.log(error));
+  }
+
+  updateEmployee() {
+    this.submitted = false;
+    this.employeeService.updateEmployee(this.id, this.employee)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.employee = new Employee();
+    this.gotoList();
+  }
+
+  save() {
+    this.employeeService.createEmployee(this.employee)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.employee = new Employee();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if(this.employee.active == null)
+      this.employee.active = false;
+
+    this.save(); 
+
+  }
+
+  gotoList() {
+    
+    this.router.navigate(['/employees']);
+  }
+}
